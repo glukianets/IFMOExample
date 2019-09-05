8c4817dd88362a4f3e3d0be2f950f999ee5a6a46
Gleb Lukianets <g.lukyanets@corp.vk.com>
2019-09-05 23:11:31 +0300

    init

---
+++ IFMOExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IFMOExample
+//
+//  Created by Глеб Лукьянец on 05/09/2019.
+//  Copyright © 2019 net.gleba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IFMOExampleTests/IFMOExampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  IFMOExampleTests.swift
+//  IFMOExampleTests
+//
+//  Created by Глеб Лукьянец on 05/09/2019.
+//  Copyright © 2019 net.gleba. All rights reserved.
+//
+
+import XCTest
+@testable import IFMOExample
+
+class IFMOExampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
